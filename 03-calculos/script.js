6b7e646bd32bbf88cc43f2cd10a8d15893014d8a
WECTON SOARES CARVALHO <wecton.scarvalho@senacsp.edu.br>
2026-02-24 11:37:51 -0300

    aula24-02

---
+++ 03-calculos/script.js
@@ -0,0 +1,20 @@
+console.log(5+7);
+console.log(3-2);
+
+const a = 5;
+console.log(a * 2)
+
+const b = 6;
+const c = 3;
+const d = b / c;
+console.log(d);
+
+const restodadivisão = b % c;
+console.log(restodadivisão);
+
+const exponenciacao = b ** c;
+console.log(exponenciacao);
+
+console.log((1+1)*4**2);
+console.log((2-2)*(3*3));
+console.log(3*2/3);
