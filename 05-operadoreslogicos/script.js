19b515c89d027858318f4bd0f776c0ef8471a967
WECTON SOARES CARVALHO <wecton.scarvalho@senacsp.edu.br>
2026-02-26 11:56:22 -0300

    atualizando

---
+++ 05-operadoreslogicos/script.js
@@ -0,0 +1,13 @@
+// operadores lógicos
+const condicao1 = true;
+const condicao2 = false;
+console.log(condicao1 && condicao2); // and
+console.log(condicao1 || condicao2); // or
+
+const condicao3 = true;
+const condicao4 = !false; // not
+console.log(condicao3 && condicao4); // and
+
+
+
+
