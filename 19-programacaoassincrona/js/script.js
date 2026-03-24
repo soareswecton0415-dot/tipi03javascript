987b781fc6cca6e98cc0d43d9b83eadf6a61a622
WECTON SOARES CARVALHO <wecton.scarvalho@senacsp.edu.br>
2026-03-24 11:59:17 -0300

    atualizando

---
+++ 19-programacaoassincrona/js/script.js
@@ -0,0 +1,30 @@
+// setTimeOut
+function exibirMensagem(){
+    console.log('A mensagem foi exibida após 6 segundos.');
+}
+
+setTimeout(exibirMensagem, 6000);
+
+// setInterval
+/*
+function exibirMensagem (){
+    console.log('A mensagem foi exibida após 3 segundos');
+}
+
+setInterval(exibirMensagem, 3000);
+*/
+
+// async function
+async function esperar(segundos){
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve(`Esperei ${segundos} segundos.`);
+        }, segundos * 1000);
+    });
+}
+
+async function executar(){
+    const resultado = await esperar(3);
+    console.log(resultado);
+}
+executar();
---
+++ 21-Classe/script.js
@@ -0,0 +1,63 @@
+// Classe
+class Pessoa {
+
+
+    // Atributos
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // método
+    saudacao(){
+        return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`
+    }
+}
+
+// instancia
+const pessoa1 = new Pessoa("Ana", 20);
+const pessoa2 = new Pessoa("Grazi", 41);
+console.log(pessoa1.saudacao());
+console.log(pessoa2.saudacao());
+
+// Novva Classe - encapsulamento
+class PessoaNova{
+    constructor(nome, idade){
+        this._nome = nome;
+        this._idade = idade;
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    set nome(novoNome){
+        this._nome = novoNome;
+    }
+}
+
+let pessoa = new PessoaNova("Ana", 20);
+console.log(pessoa.nome);
+pessoa.nome = "Maria";
+console.log(pessoa.nome);
+
+// Classe com herança
+class Animal {
+    constructor(nome){
+        this.nome = nome;
+    }
+
+    fazerSom(){
+        console.log("O animal faz um som.");
+    }
+}
+
+// subClasse
+class Cachorro extends Animal{
+    fazerSom(){
+        console.log(`O cachorro ${this.nome} late.`);
+    }
+}
+
+let rex = new Cachorro("Rex");
+rex.fazerSom(); //polimorfismo
